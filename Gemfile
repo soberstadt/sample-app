ea1f6f741ba521f84fa3a255e740d7eb40697331
Spencer Oberstadt <soberstadt@gmail.com>
2011-12-15 02:45:32 -0600

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -8,10 +8,12 @@ gem 'rails', '3.1.3'
 gem 'sqlite3'
 gem 'rake', '0.8.7'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
   gem 'rspec-rails'
   gem 'annotate'
+  gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  before_filter :already_in, :only => [:new, :create]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
   
   def new
     @title = "Sign up"
     @user = User.new
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
 
   def show
@@ -23,5 +32,50 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile update"
+      redirect_to @user
+    else
+      @title ="Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    if current_user?(user)
+      flash[:error] = "Cannot delete yourself."
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
+    redirect_to users_path
+  end
 
+  private
+  
+    def already_in
+      redirect_to(root_path) unless !signed_in?
+    end
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
